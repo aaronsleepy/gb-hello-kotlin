a9d294451ff84264bc9399e64679b9c50143e9ad
aaronsleepy <aaron@kmong.com>
2022-05-23 20:24:36 +0900

    immutable val, mutable var

--- src/main/kotlin/basic/grammar/BasicGrammar.kt
+++ src/main/kotlin/basic/grammar/BasicGrammar.kt
@@ -1,7 +1,13 @@
 package basic.grammar
 
 fun main() {
+    // immutable val
     val a = readLine()!!.toInt()
     val b = readLine()!!.toInt()
     println(a + b)
+
+    // mutable var
+    var x = 5
+    x += 1
+    print(x)
 }
